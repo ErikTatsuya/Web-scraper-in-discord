7d04c3bbd8f5f33bf99b11e36397400af66326de
Erik Tatsuya <erik.tatsuya@gmail.com>
2025-08-11 16:09:43 -0300

    there are still some errorsðŸ˜­ðŸ˜¢ðŸ˜¢

--- git_comands.ps1
+++ git_comands.ps1
@@ -1,2 +1,5 @@
+git commit -m "there are still some errors😭😢😢"
 git add .
+git branch -M main
+git remote add origin https://github.com/ErikTatsuya/Web-scraper-in-discord.git
 git pull -u origin main
